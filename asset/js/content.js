4c6ee2a66fe3178217cc9231a98decbcd2e2db7f
weichin.neoh <weichin.neoh@funeraldirectorslife.com>
2019-07-17 10:34:49 -0500

    #added about section

--- asset/js/content.js
+++ asset/js/content.js
@@ -1,42 +1,50 @@
 
-
 let last_known_scroll_position = 0;
 let ticking = false;
 let once =true;
+let second =true;
 
+$(document).ready(
+  function(){
+    window.addEventListener('scroll', function(e) {
+    last_known_scroll_position = window.scrollY;
 
-    function doSomething(scroll_pos) {
-    var img =document.getElementById("profile_img");
-    if(scroll_pos>300 && once){
-        console.log("hit");
-        fadein(img);
-        img.classList.add('horizTranslate');
-        once= false;
+    if (!ticking) {
+      window.requestAnimationFrame(function() {
+        doSomething(last_known_scroll_position);
+        ticking = false;
+      });
+
+      ticking = true;
     }
-    }
+  });
 
-window.addEventListener('scroll', function(e) {
-  last_known_scroll_position = window.scrollY;
+  function doSomething(scroll_pos) {
+      var img =document.getElementById("profile_img");
+      var menu = document.getElementById("second_menu");
+        if(scroll_pos>300 && once){
+            //fadein(img);
+            img.classList.add('horizTranslate');
+            once= false;
+        }
+        if(scroll_pos>800 && second){
+             //menu.style.display = "flex";
+             second = false;
+        }else if (scroll_pos<800 && !second){
+             //menu.style.display = "none"
+             second = true;
+        }
+  }
 
-  if (!ticking) {
-    window.requestAnimationFrame(function() {
-      doSomething(last_known_scroll_position);
-      ticking = false;
-    });
-
-    ticking = true;
+  function fadein(element) {
+      var op = 0.1;  // initial opacity
+      var timer = setInterval(function () {
+          if (op >= 1){
+              clearInterval(timer);
+          }
+          element.style.opacity = op;
+          element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+          op += op * 0.3;
+      }, 100);
   }
 });
-
-function fadein(element) {
-    var op = 0.1;  // initial opacity
-    element.style.display = 'block';
-    var timer = setInterval(function () {
-        if (op >= 1){
-            clearInterval(timer);
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op += op * 0.2;
-    }, 8);
-}
--- asset/js/myparticle.js
+++ asset/js/myparticle.js
@@ -1,172 +1,175 @@
-var canvas;
-var ctx;
-var particles = [];
-var num_particles = 200;
-var mouse = {
-    x: undefined,
-    y: undefined
-}
-var canvasW = $('body').width();
-var canvasH = $('#intro_section').height();
-var onCanvas = true;
-
-window.onload = function() {
-    init();
-};
-
-window.addEventListener('mousemove', mousePosition);
-window.addEventListener('mouseout', mouseOut);
-//window.addEventListener('scroll', mouseOut);
-//window.addEventListener('mouseenter', mousePosition);
-window.addEventListener('resize', resizeCanvas);
-
-
-/*
-* Get mouse position
-*/
-function mousePosition(event) {
-    onCanvas =true;
-    mouse.x = event.pageX;
-    mouse.y = event.pageY;
-}
-/*
-* Cancel mouse postion
-*/
-function mouseOut(){
-    onCanvas =false;
-}
-
-/*
-* Start the program
-*/
-function init(){
-    canvas = document.getElementById("canvas");
-    ctx = canvas.getContext("2d");
-    //canvas.addEventListener('mousemove', mousePosition); // doesn't work
-    canvas.width = canvasW;
-    canvas.height = canvasH;
-    start();
-};
-
-/*
-* start create particles
-*/
-function start(){
-    for (var i = 0; i < num_particles; i++){
-        particles.push(new Particle());
-       
+$(document).ready(function(){
+    var canvas;
+    var ctx;
+    var particles = [];
+    var num_particles = 200;
+    var mouse = {
+        x: undefined,
+        y: undefined
     }
-    loop();
-}
+    var canvasW = $('body').width();
+    var canvasH = $('#intro_section').height();
+    var onCanvas = true;
 
-/*
-* Particle Object
-*/
-var Particle = function () {
-    this.x = canvas.width * Math.random();
-    this.y = canvas.height * Math.random();
-   
-    if(Math.random() >= 0.5){
-        this.vx = Math.random()*2;
-    }else{
-        this.vx = Math.random()*-2;
+    window.onload = function() {
+        init();
+    };
+
+    window.addEventListener('mousemove', mousePosition);
+    //canvas.addEventListener('mouseout', mouseOut);
+    window.addEventListener('scroll', mouseOut);
+    //window.addEventListener('mouseenter', mousePosition);
+    window.addEventListener('resize', resizeCanvas);
+
+
+    /*
+    * Get mouse position
+    */
+    function mousePosition(event) {
+        onCanvas =true;
+        mouse.x = event.pageX;
+        mouse.y = event.pageY;
     }
-   
-    if(Math.random() >= 0.5){
-        this.vy = Math.random()*2;
-    }else{
-        this.vy = Math.random()*-2;
+    /*
+    * Cancel mouse postion
+    */
+    function mouseOut(){
+        onCanvas =false;
     }
 
-    this.Color = "#3a506b";
-    this.radius = 2.5;
-
-    this.draw = function () {
-        ctx.beginPath();
-        ctx.arc(this.x,this.y,this.radius, 0, 2 * Math.PI);
-        ctx.fillStyle = this.Color;
-        ctx.fill();
+    /*
+    * Start the program
+    */
+    function init(){
+        canvas = document.getElementById("canvas");
+        ctx = canvas.getContext("2d");
+        //canvas.addEventListener('mousemove', mousePosition); // doesn't work
+        canvas.addEventListener('mouseout', mouseOut);
+        canvas.width = canvasW;
+        canvas.height = canvasH;
+        start();
     };
 
     /*
-    * check if it's out of boundary
+    * start create particles
     */
-    this.update = function () {
-        this.x += this.vx;
-        this.y += this.vy;
- 
-       
-        if (this.x<0 || this.x > canvas.width){
-            this.vx = -this.vx;
+    function start(){
+        for (var i = 0; i < num_particles; i++){
+            particles.push(new Particle());
+        
         }
-   
-        if (this.y < 0 || this.y > canvas.height){
-            this.vy = -this.vy;
-        }
-    };
+        loop();
+    }
+
     /*
-    * get distance between
+    * Particle Object
     */
-    this.distanceBetween = function(p){
-    var dx = this.x -p.x;
-    var dy = this.y -p.y;
-
-    return Math.sqrt(dx * dx + dy * dy);
-    };
-}
-
-/*
-* loop to get condition
-*/
-function loop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    for(var i = 0;i<num_particles;i++){
-        particles[i].draw();
-        particles[i].update();
-
-        if(particles[i].distanceBetween(mouse)<150 && onCanvas){
-            connect(particles[i],mouse,particles[i].distanceBetween(mouse),mouse);
+    var Particle = function () {
+        this.x = canvas.width * Math.random();
+        this.y = canvas.height * Math.random();
+    
+        if(Math.random() >= 0.5){
+            this.vx = Math.random()*2;
+        }else{
+            this.vx = Math.random()*-2;
         }
-        for (var j = 0; j < num_particles; j++) {
-            if(particles[i].distanceBetween(particles[j])<105){
-                connect(particles[i],particles[j],particles[i].distanceBetween(particles[j]));
+    
+        if(Math.random() >= 0.5){
+            this.vy = Math.random()*2;
+        }else{
+            this.vy = Math.random()*-2;
+        }
+
+        this.Color = "#3a506b";
+        this.radius = 2.5;
+
+        this.draw = function () {
+            ctx.beginPath();
+            ctx.arc(this.x,this.y,this.radius, 0, 2 * Math.PI);
+            ctx.fillStyle = this.Color;
+            ctx.fill();
+        };
+
+        /*
+        * check if it's out of boundary
+        */
+        this.update = function () {
+            this.x += this.vx;
+            this.y += this.vy;
+    
+        
+            if (this.x<0 || this.x > canvas.width){
+                this.vx = -this.vx;
+            }
+    
+            if (this.y < 0 || this.y > canvas.height){
+                this.vy = -this.vy;
+            }
+        };
+        /*
+        * get distance between
+        */
+        this.distanceBetween = function(p){
+        var dx = this.x -p.x;
+        var dy = this.y -p.y;
+
+        return Math.sqrt(dx * dx + dy * dy);
+        };
+    }
+
+    /*
+    * loop to get condition
+    */
+    function loop() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        for(var i = 0;i<num_particles;i++){
+            particles[i].draw();
+            particles[i].update();
+
+            if(particles[i].distanceBetween(mouse)<150 && onCanvas){
+                connect(particles[i],mouse,particles[i].distanceBetween(mouse),mouse);
+            }
+            for (var j = 0; j < num_particles; j++) {
+                if(particles[i].distanceBetween(particles[j])<105){
+                    connect(particles[i],particles[j],particles[i].distanceBetween(particles[j]));
+                }
             }
         }
+        requestAnimationFrame(loop);
     }
-    requestAnimationFrame(loop);
-}
 
-/*
-* resize canvas
-*/
-function resizeCanvas()
-{  
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    for(var i = 0;i<num_particles;i++){
-        particles[i].draw();
+    /*
+    * resize canvas
+    */
+    function resizeCanvas()
+    {  
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        for(var i = 0;i<num_particles;i++){
+            particles[i].draw();
+        }
     }
-}
 
 
-/*
-* connect two point with line
-*/
-function connect(fp,sp,distance,object){
-    if(object ===mouse){
-        ctx.beginPath();
-        ctx.strokeStyle = 'rgba(92, 107, 115,2.5)';
-        ctx.moveTo(fp.x, fp.y); // begin at position (fp.x,fp.y)
-        ctx.lineTo(sp.x, sp.y); // ends at position (sp.x,sp.y)
-        ctx.stroke();
-        ctx.closePath();
-    }else{
-        ctx.beginPath();
-        ctx.strokeStyle = 'rgba(92, 107, 115,'+Math.min(20/distance,1)+')';
-        ctx.moveTo(fp.x, fp.y); // begin at position (fp.x,fp.y)
-        ctx.lineTo(sp.x, sp.y); // ends at position (sp.x,sp.y)
-        ctx.stroke();
-        ctx.closePath();
+    /*
+    * connect two point with line
+    */
+    function connect(fp,sp,distance,object){
+        if(object ===mouse){
+            ctx.beginPath();
+            ctx.strokeStyle = 'rgba(92, 107, 115,2.5)';
+            ctx.moveTo(fp.x, fp.y); // begin at position (fp.x,fp.y)
+            ctx.lineTo(sp.x, sp.y); // ends at position (sp.x,sp.y)
+            ctx.stroke();
+            ctx.closePath();
+        }else{
+            ctx.beginPath();
+            ctx.strokeStyle = 'rgba(92, 107, 115,'+Math.min(20/distance,1)+')';
+            ctx.moveTo(fp.x, fp.y); // begin at position (fp.x,fp.y)
+            ctx.lineTo(sp.x, sp.y); // ends at position (sp.x,sp.y)
+            ctx.stroke();
+            ctx.closePath();
+        }
     }
-}
+});
